8c557588afb43c15724e3f07a97eac0d16cab19e
Vitalyazavr <vitalis03@mail.ru>
2020-03-03 20:06:16 +0300

    Add 3rd task

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="snapshot",
+    packages=setuptools.find_packages(),
+    scripts=['snapshot'],
+    version="1.0",
+    author="Vitali Andrushkevich",
+    author_email="Vitali_Andrushkevich@epam.com",
+    description="PC info to json or txt"
+)
