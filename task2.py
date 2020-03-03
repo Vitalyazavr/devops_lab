2349a8b2bae7e6d32853b986c9343eb3b73bdea5
Vitaly Andrushkevich <46499807+Vitalyazavr@users.noreply.github.com>
2020-03-03 10:16:17 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -6,11 +6,7 @@ delim = len(my_string) // 2
 
 for i in range(delim):
     if my_string[i] == my_string[-(i + 1)]:
-<<<<<<< HEAD
         count += 1
-=======
-        count = count + 1
->>>>>>> a2aa397b14db1dea09602c03135efeaf890629c2
     else:
         print('No')
         break
