2a63a1b2835fe62c8926dc20dff983e396d589e1
Vitalyazavr <vitalis03@mail.ru>
2020-03-03 10:01:23 +0300

    Fixed issues

--- task4.py
+++ task4.py
@@ -5,10 +5,17 @@ list1 = []
 
 for i in range(9, 1, -1):
     if key < 10 and key == i:
+<<<<<<< HEAD
+        i -= 1
+    else:
+        while inpt % i == 0:
+            inpt /= i
+=======
         i = i - 1
     else:
         while inpt % i == 0:
             inpt = inpt / i
+>>>>>>> a2aa397b14db1dea09602c03135efeaf890629c2
             list1.append(str(i))
 
 if len(list1) <= 1:
--- task5.py
+++ task5.py
@@ -9,7 +9,12 @@ for line in range(key):
     list1.append((input()).split())
 
 for i in range(len(list1)):
+<<<<<<< HEAD
+    sum1 += int(list1[i][i])
+    sum2 += int(list1[i][-1 * (i + 1)])
+=======
     sum1 = sum1 + int(list1[i][i])
     sum2 = sum2 + int(list1[i][-1 * (i + 1)])
+>>>>>>> a2aa397b14db1dea09602c03135efeaf890629c2
 
 print(abs(sum1 - sum2))
