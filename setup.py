091931dd1ec8ce1d47aa83e81ac3471984e52e07
Vitalyazavr <vitalis03@mail.ru>
2020-03-04 20:47:17 +0300

    completed 4th task

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pr-stats",
+    packages=setuptools.find_packages(),
+    scripts=['pr-stats'],
+    version="v0.1_beta",
+    author="Vitali Andrushkevich",
+    author_email="Vitali_Andrushkevich@epam.com",
+    description="GitHub info"
+)
